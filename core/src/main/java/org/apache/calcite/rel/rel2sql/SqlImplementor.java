9716137b38dcc01dfcf2de21582a82a74355963e
pavelgubin <pavel@contiamo.com>
2019-07-01 14:09:48 +0200

    Adding column aliases to subquery in order to be able to address them in outer query (#9)

--- core/src/main/java/org/apache/calcite/rel/rel2sql/SqlImplementor.java
+++ core/src/main/java/org/apache/calcite/rel/rel2sql/SqlImplementor.java
@@ -1213,10 +1213,34 @@ public abstract class SqlImplementor {
      * equivalent to "SELECT * FROM emp AS emp".) */
     public SqlNode asFrom() {
       if (neededAlias != null) {
-        return SqlStdOperatorTable.AS.createCall(POS, node,
-            new SqlIdentifier(neededAlias, POS));
+        SqlNode n = node;
+        // hack to fix 'missing alias in postgres FROM clause'. Need proper fix
+        if (node.getKind() == SqlKind.AS) {
+          n = ((SqlCall) node).operand(0);
+        }
+        addColumnAliases(n);
+        return SqlStdOperatorTable.AS.createCall(POS, n, new SqlIdentifier(neededAlias, POS));
       }
       return node;
+    }
+
+    /** Adds aliases to columns named implicitly as EXPR$0, EXPR$1, ... */
+    private void addColumnAliases(SqlNode node) {
+      if (node instanceof SqlSelect && neededType != null) {
+        SqlNodeList selectList = ((SqlSelect) node).getSelectList();
+        if (selectList != null) { // select * doesn't have select list
+          for (int i = 0; i < selectList.size(); i++) {
+            String name = neededType.getFieldNames().get(i);
+            if (name.startsWith("EXPR$")) {
+              SqlNode e = selectList.get(i);
+              SqlNode aliased = SqlStdOperatorTable.AS.createCall(
+                  POS, e, new SqlIdentifier(name, POS));
+              selectList.set(i, aliased);
+              ordinalMap.remove(name.toLowerCase(Locale.ROOT));
+            }
+          }
+        }
+      }
     }
 
     public SqlSelect subSelect() {
